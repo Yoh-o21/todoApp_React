437b8244072d0086db3a655dcf47184581ed5098
Yoh <75738518+Yoh-o21@users.noreply.github.com>
2021-03-11 11:44:27 +0900

    CSS in JS

--- src/components/CompleteTodos.jsx
+++ src/components/CompleteTodos.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 export const CompleteTodos = (props) => {
+  const style = {
+    backgroundColor: "#ffffe0",
+    width: "400px",
+    minHeight: "200px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+  };
   const { todos, onClickBack } = props;
   return (
-    <div className="complete-area">
+    <div style={style}>
       <p className="title">完了済みのTODO</p>
       <ul>
         {todos.map((todo, index) => {
